e020aca89bb1e452207d0ca9fe5a8a5604c291fb
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-09-10 17:05:41 +0300

    views started

--- papukaaniApp/urls.py
+++ papukaaniApp/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /papukaani/
     url(r'^$', views.index, name='index'),
-    url(r'^public/', views.public, name='index')
+    url(r'^public/', views.public, name='index'),
+    # ex: /papukaani/creature/1/
+    url(r'^creature/(?P<creature_id>[0-9]+)/$', views.creature, name='creature'),
+    # ex: /papukaani/creatures/
+    url(r'^creatures/$', views.creatures, name='creatures')
 ]
--- papukaaniApp/views.py
+++ papukaaniApp/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Creature
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'papukaaniApp/index.html')
 
 def public(request):
-    return render(request, 'public.html')
+    return render(request, 'papukaaniApp/public.html')
+
+def creatures(request):
+
+    demcreature = Creature() # placeholder
+    demcreature.name = "Koecreature"
+    demcreature.id = 1
+    demcreature2 = Creature() # placeholder
+    demcreature2.name = "Koecreature2"
+    demcreature2.id = 2
+
+    context = {
+        'creature_list': [demcreature, demcreature2]
+    }
+
+    return render(request, 'papukaaniApp/creatures.html', context)
+
+def creature(request, creature_id):
+
+    # creature = get_object_or_404(Creature, pk=creature_id)
+    demcreature = Creature() # placeholder
+    demcreature.name = "Koecreature"
+    context = {
+        'creature_id': creature_id,
+        'creature': demcreature
+    }
+
+    return render(request, 'papukaaniApp/creature.html', context)

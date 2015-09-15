bcc942fbf67d841258c09c19d8f80f14322af90a
Rapsutin <juho.lamminmaki@gmail.com>
2015-09-15 15:45:57 +0300

    Removed useless django_jenkins -plugin

--- papukaani/settings.py
+++ papukaani/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'papukaaniApp',
-    'django_jenkins'
+    'papukaaniApp'
 )
 
 MIDDLEWARE_CLASSES = (

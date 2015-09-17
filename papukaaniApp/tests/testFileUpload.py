5c33b9f62811c95cb067471a77cc3279e34c2c5c
Iiro Aalto <iiroaalto@gmail.com>
2015-09-17 10:30:20 +0300

    Tests, tests, refactor & error messages

--- papukaaniApp/tests/testFileUpload.py
+++ papukaaniApp/tests/testFileUpload.py
@@ -4,24 +4,41 @@ from papukaaniApp.models import MapPoint
 from unittest import mock
 from datetime import datetime
 
+_filePath = "papukaaniApp/tests/test_files/"
+
 class FileUploadTest(TestCase):
     def setUp(self):
         self.c = Client()
 
-    def test_get_to_index_returns_200(self):
-        response = self.c.get('/papukaani/')
+    def test_get_to_upload_returns_200(self):
+        response = self.c.get('/papukaani/upload/')
         self.assertTrue(response.status_code == 200)
 
     def test_post_to_upload_is_redirected(self):
         response = self.c.get('/papukaani/upload/')
-        self.assertTrue(response.status_code == 302)
+
+        self.assertTrue(response.status_code == 200)
 
     def test_post_to_upload_with_file_creates_database_entry(self):
         before = MapPoint.objects.all().count()
 
-        with open("ecotones.csv") as file:
+        with open(_filePath+"ecotones.csv") as file:
             response = self.c.post('/papukaani/upload/', {'file' : file})
 
         after = MapPoint.objects.all().count()
         self.assertTrue(after > before)
+
+    def test_file_upload_response_contains_json(self):
+        with open(_filePath + "ecotones.csv") as file:
+            response = self.c.post('/papukaani/upload/', {'file' : file})
+
+        self.assertTrue("latitude" in  str(response.content))
+        self.assertTrue("[{" in  str(response.content))
+        self.assertTrue("longitude" in  str(response.content))
+
+    def test_invalid_file_does_not_cause_exception(self):
+        with open(_filePath+"invalid.txt") as file:
+            response = self.c.post('/papukaani/upload/', {'file' : file})
+
+        self.assertTrue(response.status_code == 302)
 
--- papukaaniApp/views.py
+++ papukaaniApp/views.py
@@ -8,18 +8,27 @@ from .utils.parser import ecotones_parse
 def index(request):
     return render(request, 'index.html')
 
-
 def public(request):
     return render(request, 'public.html')
 
 
 def upload(request):
+    if request.method == 'GET':
+        message = request.GET['m'] if 'm' in request.GET else ''
+        return render(request, 'upload.html', {"message" : message})
     if request.method == 'POST':
         if 'file' in request.FILES:
             file = request.FILES['file']
-            data = ecotones_parse(file)
+            try:
+                data = ecotones_parse(file)
+            except:
+                return _redirect_with_param(upload, "?m=Tiedostosi formaatti ei ole kelvollinen!")
             points = [MapPoint.objects.create(**point) for point in data]
-            return render(request, 'index.html', {'points': serialize('json', points)})
-        return redirect(index)
-    return redirect(index)
+            return render(request, 'upload.html', {'points': serialize('json', points)})
 
+        return _redirect_with_param(upload, "?m=Et valinnut ladattavaa tiedostoa!")
+
+def _redirect_with_param(to, param):
+    response = redirect(to)
+    response['Location'] += param
+    return response

e8486765214eefbfd4732faff2b64c9667966182
Uskon <henri.vehmas@helsinki.fi>
2015-09-17 12:00:44 +0300

    Merge

--- papukaaniApp/views.py
+++ papukaaniApp/views.py
@@ -7,10 +7,12 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import Creature
+import timeit
 
 
 def index(request):
     return render(request, 'papukaaniApp/index.html')
+
 
 def public(request):
     return render(request, 'papukaaniApp/public.html')
@@ -19,7 +21,7 @@ def public(request):
 def upload(request):
     if request.method == 'GET':
         message = request.GET['m'] if 'm' in request.GET else ''
-        return render(request, 'upload.html', {"message" : message})
+        return render(request, 'upload.html', {"message": message})
     if request.method == 'POST':
         if 'file' in request.FILES:
             file = request.FILES['file']
@@ -27,22 +29,26 @@ def upload(request):
                 data = ecotones_parse(file)
             except:
                 return _redirect_with_param(upload, "?m=Tiedostosi formaatti ei ole kelvollinen!")
-            points = [MapPoint.objects.create(**point) for point in data]
-            return render(request, 'upload.html', {'points': serialize('json', points)})
+            points = [MapPoint(**point) for point in data]
+            MapPoint.objects.bulk_create(points)
+
+            latlongs = [[mapPoint.latitude, mapPoint.longitude] for mapPoint in points]
+            return render(request, 'choose.html', {'points': latlongs})
 
         return _redirect_with_param(upload, "?m=Et valinnut ladattavaa tiedostoa!")
+
 
 def _redirect_with_param(to, param):
     response = redirect(to)
     response['Location'] += param
     return response
 
-def creatures(request):
 
-    demcreature = Creature() # placeholder
+def creatures(request):
+    demcreature = Creature()  # placeholder
     demcreature.name = "Koecreature"
     demcreature.id = 1
-    demcreature2 = Creature() # placeholder
+    demcreature2 = Creature()  # placeholder
     demcreature2.name = "Koecreature2"
     demcreature2.id = 2
 
@@ -52,10 +58,10 @@ def creatures(request):
 
     return render(request, 'papukaaniApp/creatures.html', context)
 
-def creature(request, creature_id):
 
+def creature(request, creature_id):
     # creature = get_object_or_404(Creature, pk=creature_id)
-    demcreature = Creature() # placeholder
+    demcreature = Creature()  # placeholder
     demcreature.name = "Koecreature"
     context = {
         'creature_id': creature_id,

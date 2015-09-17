50a08a16168419ca65f66fbc4bb52f71c064d20e
Uskon <henri.vehmas@helsinki.fi>
2015-09-17 13:10:04 +0300

    Merge

---
+++ papukaaniApp/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Creature',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=300)),
+                ('gpsNumber', models.IntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='MapPoint',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('timestamp', models.DateTimeField()),
+                ('latitude', models.DecimalField(max_digits=12, decimal_places=9)),
+                ('longitude', models.DecimalField(max_digits=12, decimal_places=9)),
+                ('altitude', models.DecimalField(max_digits=8, decimal_places=3)),
+                ('temperature', models.DecimalField(max_digits=5, decimal_places=2)),
+                ('public', models.BooleanField()),
+                ('creature', models.ForeignKey(to='papukaaniApp.Creature')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='mappoint',
+            unique_together=set([('creature', 'timestamp')]),
+        ),
+    ]

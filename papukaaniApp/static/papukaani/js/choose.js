c6fcc440ca88792d73854429dbcd5828228334a9
Iiro Aalto <iiroaalto@gmail.com>
2015-09-22 16:23:53 +0300

    Choose public points frontend

--- papukaaniApp/static/papukaani/js/choose.js
+++ papukaaniApp/static/papukaani/js/choose.js
@@ -2,18 +2,31 @@ function init(data){
     points = JSON.parse(data); //Format : {latlong : [x,y], id : int}
     map = create_map("map", [61.0, 20.0], 5)
 
-    var latlongs = points.map(function(currentValue, index, array){
-        return currentValue.latlong
+    markers = L.markerClusterGroup({zoomToBoundsOnClick: false});
+
+    markers.on('clusterdblclick', function(a){
+        var mkrs = a.layer.getAllChildMarkers()
+        for(var i = 0; i < mkrs.length; i++){
+            mkrs[i].pnt.public = !mkrs[i].pnt.public;
+        }
     })
 
-    if(latlongs){
-        L.polyline(latlongs, {smoothFactor: 25.0}).addTo(map);
+    for(var i = 0; i < points.length; i++){
+        var ltlgs = points[i].latlong;
+        var marker = L.marker(new L.LatLng(ltlgs[0],ltlgs[1]));
+        marker.pnt = points[i]
+
+        marker.on('dblclick', function(p){
+            return function(e){
+                p.public = !p.public;
+            }
+        }(points[i]));
+
+        markers.addLayer(marker);
     }
+
+    map.addLayer(markers)
 }
 
-function drawPublicPoints(){
-    var latlongs = points.map(function(currentValue, index, array){
-        
-    })
 
-}
+

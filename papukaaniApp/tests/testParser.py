872e5b3934a8d03f61db09e709713d1e6a6e49f3
Uskon <henri.vehmas@helsinki.fi>
2015-09-22 12:13:12 +0300

    Parser tests

--- papukaaniApp/tests/testParser.py
+++ papukaaniApp/tests/testParser.py
@@ -1,6 +1,7 @@
 from papukaaniApp.utils.parser import ecotones_parse
 from django.test import TestCase
 from django.conf import settings
+from papukaaniApp.models import Creature, MapPoint
 
 
 class FileParserTest(TestCase):
@@ -14,3 +15,14 @@ class FileParserTest(TestCase):
         for entry in entries:
             assert str(lats[i]) in str(entry["latitude"])
             i += 1
+
+    def test_points_can_be_succesfully_created_from_parsed_contents(self):
+        path = settings.OTHER_ROOT + "/Ecotones_gps_pos_test.csv"
+        file = open(path)
+        entries = ecotones_parse(file)
+        points = []
+        for entry in entries:
+            creature, was_created = Creature.objects.get_or_create(name="Pekka")
+            point = MapPoint(creature=creature, **entry)
+            points.append(point)
+        assert len(points) == 5

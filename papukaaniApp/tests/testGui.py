448ea5ecde352c002afba69fcc217dd373a30195
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-09-22 16:39:37 +0300

    gui enchancements and first test

---
+++ papukaaniApp/tests/testGui.py
@@ -0,0 +1,25 @@
+from time import sleep
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+from django.test.testcases import LiveServerTestCase
+
+from selenium import webdriver
+
+from papukaaniApp.tests.page_models import MainNavigation
+from papukaaniApp.views.upload_views import upload
+
+
+class TestGui(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.base_url = 'http://127.0.0.1:8081'
+        self.browser.get(self.base_url + '/papukaani')
+
+    def test(self):
+        MainNavigation(self.browser).openPage(upload)
+        sleep(5)
+        self.assertEquals(self.browser.current_url, self.base_url + reverse("upload"))
+    
+
+

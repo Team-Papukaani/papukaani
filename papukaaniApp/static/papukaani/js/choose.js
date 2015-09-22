e791dec74fce2cf39fa0ef99e4f9a40d3ea1287b
Iiro Aalto <iiroaalto@gmail.com>
2015-09-22 17:53:22 +0300

    Can save public points from view

--- papukaaniApp/static/papukaani/js/choose.js
+++ papukaaniApp/static/papukaani/js/choose.js
@@ -2,31 +2,69 @@ function init(data){
     points = JSON.parse(data); //Format : {latlong : [x,y], id : int}
     map = create_map("map", [61.0, 20.0], 5)
 
-    markers = L.markerClusterGroup({zoomToBoundsOnClick: false});
+     blueIcon = L.icon({
+        iconUrl: "/static/papukaani/media/blueMarker.png",
+        iconSize: [38, 38],
+        iconAnchor: [19, 38],
+        popupAnchor: [-3, -76],
+     });
+
+     greyIcon = L.icon({
+        iconUrl: "/static/papukaani/media/greyMarker.png",
+        iconSize: [38, 38],
+        iconAnchor: [19, 38],
+        popupAnchor: [-3, -76],
+     });
+
+
+
+    markers = L.markerClusterGroup({
+        zoomToBoundsOnClick: false,
+        maxClusterRadius : 40,
+        disableClusteringAtZoom : 13
+    });
 
     markers.on('clusterdblclick', function(a){
         var mkrs = a.layer.getAllChildMarkers()
         for(var i = 0; i < mkrs.length; i++){
-            mkrs[i].pnt.public = !mkrs[i].pnt.public;
+            changePublicity(mkrs[i]);
         }
     })
 
     for(var i = 0; i < points.length; i++){
         var ltlgs = points[i].latlong;
         var marker = L.marker(new L.LatLng(ltlgs[0],ltlgs[1]));
-        marker.pnt = points[i]
+        marker.pnt = points[i];
+        chooseIcon(marker);
 
-        marker.on('dblclick', function(p){
+        marker.on('dblclick', function(marker){
             return function(e){
-                p.public = !p.public;
+                changePublicity(marker);
             }
-        }(points[i]));
+        }(marker));
 
         markers.addLayer(marker);
     }
 
-    map.addLayer(markers)
+    map.addLayer(markers);
 }
 
+function chooseIcon(marker){
+    icon = marker.pnt.public ? blueIcon : greyIcon;
+    marker.setIcon(icon);
+}
 
+function changePublicity(marker){
+    marker.pnt.public = !marker.pnt.public;
+    chooseIcon(marker);
+}
 
+function send(csrf_token){
+    data = JSON.stringify(points)
+    xmlhttp = new XMLHttpRequest;
+
+    xmlhttp.open("POST","", true);
+    xmlhttp.setRequestHeader("X-CSRFToken", csrf_token);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("data="+data);
+}

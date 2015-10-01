c2a7f86831f1581d4ce49747909b0fd0c0df92fd
Uskon <henri.vehmas@helsinki.fi>
2015-10-01 12:46:53 +0300

    Replaced phantomjs with headless firefox

--- papukaaniApp/tests/page_models/page_model.py
+++ papukaaniApp/tests/page_models/page_model.py
@@ -1,21 +1,29 @@
 from selenium.webdriver.support.wait import WebDriverWait
+from pyvirtualdisplay import Display
+from selenium import webdriver
 
 
 class Page:
     """
     Basic Page Object to be used as super class.
     """
-
+    display = None
     url = None
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self):
+        self.display = Display(visible=0, size=(800, 600))
+        self.display.start()
+        self.driver = webdriver.Firefox()
 
     def navigate(self):
         """
         Proceed the driver to the set url.
         """
         self.driver.get(self.url)
+
+    def close(self):
+        self.driver.close()
+        self.display.stop()
 
 
 class Element:
--- papukaaniApp/tests/testPublicView.py
+++ papukaaniApp/tests/testPublicView.py
@@ -1,6 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import Client
-from selenium import webdriver
 
 from papukaaniApp.tests.page_models.page_models import PublicPage
 from papukaani.settings import *
@@ -8,16 +7,10 @@ from papukaaniApp.models import MapPoint
 
 
 class PublicView(StaticLiveServerTestCase):
-    def setUp(self):
-        self.driver = webdriver.PhantomJS()
-        self.c = Client()
-
-    def tearDown(self):
-        self.driver.close()
 
     def test_public_points_are_shown_on_map(self):
         with open(BASE_DIR + "/papukaaniApp/tests/test_files/ecotones.csv") as file:
-            self.c.post('/papukaani/upload/', {'file': file})
+            Client().post('/papukaani/upload/', {'file': file})
 
         points = MapPoint.objects.all()
         len(points)
@@ -26,8 +19,9 @@ class PublicView(StaticLiveServerTestCase):
             points[num].public = 1
             points[num].save()
 
-        public = PublicPage(self.driver, points[1].creature.id)
+        public = PublicPage(points[1].creature.id)
         public.navigate()
 
         self.assertEquals(public.get_points_json().count('['), 5)  # 4 JSON points + 1 for the containing array
         self.assertNotEquals(public.get_map_polyline_elements(), None)  # <g> includes polylines on map
+        public.close()

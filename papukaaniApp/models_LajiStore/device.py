52182bf9845bf34b865694d3a29c754f37932038
Iiro Aalto <iiroaalto@gmail.com>
2015-10-06 18:14:47 +0300

    Documentation for devices and documents

--- papukaaniApp/models_LajiStore/device.py
+++ papukaaniApp/models_LajiStore/device.py
@@ -1,44 +1,75 @@
 from papukaaniApp.services.lajistore_service import LajiStoreAPI
 
+
 class Device():
+    '''
+    Represents the Device table of LajiStore
+    '''
+    def __init__(self, id, deviceId, deviceType, deviceManufacturer, createdAt, createdBy, lastModifiedAt,
+                 lastModifiedBy, facts, **kwargs):
+        self.id = id
+        self.deviceId = deviceId
+        self.deviceType = deviceType
+        self.deviceManufacturer = deviceManufacturer
+        self.createdAt = createdAt
+        self.createdBy = createdBy
+        self.lastModifiedAt = lastModifiedAt
+        self.lastModifiedBy = lastModifiedBy
+        self.facts = facts
 
-	def __init__(self, id, deviceId, deviceType, deviceManufacturer, createdAt, createdBy, lastModifiedAt, lastModifiedBy, facts, **kwargs):
-		self.id = id		
-		self.deviceId = deviceId
-		self.deviceType = deviceType
-		self.deviceManufacturer = deviceManufacturer
-		self.createdAt = createdAt
-		self.createdBy = createdBy
-		self.lastModifiedAt = lastModifiedAt
-		self.lastModifiedBy = lastModifiedBy
-		self.facts = facts
+    def delete(self):
+        '''
+        Deletes the device from LajiStore. Note that the object is not destroyed!
+        '''
+        LajiStoreAPI.delete_device(self.id)
 
-	def delete(self):
-		LajiStoreAPI.delete_device(self.id)
+    def update(self):
+        '''
+        Saves changes to the object to the corresponding LajiStore entry.
+        '''
+        LajiStoreAPI.update_device(**self.__dict__)  # __dict__ puts all arguments here
 
-	def update(self):
-		LajiStoreAPI.update_device(**self.__dict__) #__dict__ puts all arguments here
 
 def find(**kwargs):
-	return _get_many(**kwargs)
+    '''
+    Find all matching devices.
+    :param kwargs: Search parameters.
+    :return: A list of Device objects.
+    '''
+    return _get_many(**kwargs)
+
 
 def get_all():
-	return _get_many()
+    '''
+    Returns all devices
+    :return A list of Device objects:
+    '''
+    return _get_many()
+
 
 def get(deviceId):
-	device = LajiStoreAPI.get_device(deviceId)
-	return Device(**device)
+    '''
+    Gets a device from LajiStore
+    :param id: The LajiStore ID of the device
+    :return: A Device object
+    '''
+    device = LajiStoreAPI.get_device(deviceId)
+    return Device(**device)
+
 
 def create(deviceId, deviceType, deviceManufacturer, createdAt, createdBy, lastModifiedAt, lastModifiedBy, facts):
-	device = LajiStoreAPI.post_device(deviceId, deviceType, deviceManufacturer, createdAt, createdBy, lastModifiedAt, lastModifiedBy, facts)
-	return Device(**device)
+    '''
+    Creates a device instance in LajiStore and a corresponding Device object
+    :return: A Device object
+    '''
+    device = LajiStoreAPI.post_device(deviceId, deviceType, deviceManufacturer, createdAt, createdBy, lastModifiedAt,
+                                      lastModifiedBy, facts)
+    return Device(**device)
+
 
 def _get_many(**kwargs):
-	data = LajiStoreAPI.get_all_devices(**kwargs)
-	devices = []
-	for device in data:	#creates a list of devices to return
-		devices.append(Device(**device))
-	return devices
-
-
-
+    data = LajiStoreAPI.get_all_devices(**kwargs)
+    devices = []
+    for device in data:  # creates a list of devices to return
+        devices.append(Device(**device))
+    return devices
--- papukaaniApp/models_LajiStore/document.py
+++ papukaaniApp/models_LajiStore/document.py
@@ -2,6 +2,9 @@ from papukaaniApp.services.lajistore_service import LajiStoreAPI
 from papukaaniApp.models_LajiStore import gathering
 
 class Document():
+    '''
+    Represents the LajiStore table Document
+    '''
     def __init__(self, id, documentId, lastModifiedAt, lastModifiedBy, createdAt, createdBy, facts, gatherings, **kwargs):
         self.id = id
         self.documentId = documentId
@@ -13,25 +16,48 @@ class Document():
         self.gatherings = _parse_gathering(gatherings)
 
     def delete(self):
+        '''
+        Deletes the document from LajiStore. Note that the object is not destroyed!
+        '''
         LajiStoreAPI.delete_document(self.id)
 
     def update(self):
+        '''
+        Saves changes to the object to the corresponding LajiStore entry.
+        '''
         dict = self.__dict__
         dict["gatherings"] = [g.to_lajistore_json() for g in self.gatherings]
         LajiStoreAPI.update_document(**dict) #__dict__ puts all arguments here
 
 def find(**kwargs):
+    '''
+    Find all matching documents.
+    :param kwargs: Search parameters.
+    :return: A list of Document objects.
+    '''
     return _get_many(**kwargs)
 
 def get_all():
+    '''
+    Returns all documents
+    :return A list of Document objects:
+    '''
     return _get_many()
 
-def get(documentID):
-    document = LajiStoreAPI.get_document(documentID)
+def get(id):
+    '''
+    Gets a document from LajiStore
+    :param id: The LajiStore ID of the document
+    :return: A Document object
+    '''
+    document = LajiStoreAPI.get_document(id)
     return Document(**document)
 
 def create(documentId, lastModifiedAt, lastModifiedBy, createdAt, createdBy, facts, gatherings):
-
+    '''
+    Creates a document instance in LajiStore and a corresponding Document object
+    :return: A Document object
+    '''
     document = LajiStoreAPI.post_document(documentId, lastModifiedAt, lastModifiedBy, createdAt, createdBy, facts, gatherings)
     return Document(**document)
 

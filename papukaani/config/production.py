0f68e778bfd1cd139c7edf71218a28714a5a1de3
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2015-10-06 14:48:05 +0300

    Fixed production secret_key

--- papukaani/config/production.py
+++ papukaani/config/production.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from .common import *
 import os
 
-SECRET_KEY = os.environ("PAPUKAANI_SECRET_KEY")
+SECRET_KEY = os.environ["PAPUKAANI_SECRET_KEY"]
 
 DEBUG = False
 

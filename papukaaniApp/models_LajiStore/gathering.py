d420fc3f403c4d5a8c931ae7473fd69b92acee8d
Iiro Aalto <iiroaalto@gmail.com>
2015-10-06 18:22:34 +0300

    Gathering documented

--- papukaaniApp/models_LajiStore/gathering.py
+++ papukaaniApp/models_LajiStore/gathering.py
@@ -1,5 +1,7 @@
 class Gathering():
-
+    '''
+    Represets the gatherings in a Document table in LajiStore
+    '''
     def __init__(self, time, geometry, temperature, facts):
         self.time = time
         self.geometry = geometry
@@ -7,10 +9,19 @@ class Gathering():
         self.facts = facts
 
     def to_lajistore_json(self):
+        '''
+        Returns the fields in a LajiStore-saveable format.
+        :return: A dictionary
+        '''
         return {"timeStart":self.time, "wgs84Geometry":{ "type":"Point", "coordinates" : self.geometry}, "temperatureCelsius":self.temperature, "facts":self.facts}
 
 
 def from_lajistore_json(**kwargs):
+    '''
+    Creates a Gathering object from LajiStore-format json
+    :param kwargs: The data from LajiStore
+    :return: a Gathering object
+    '''
     return Gathering(kwargs["timeStart"], kwargs["wgs84Geometry"]["coordinates"], kwargs["temperatureCelsius"], kwargs["facts"])
 
 

c40c845fa75a5bfba748655f76e2f7a7681b40ef
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-10-06 10:55:32 +0300

    public view started

--- papukaaniApp/views/creature_views.py
+++ papukaaniApp/views/creature_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from papukaaniApp.models import Creature, MapPoint
 import json
+from papukaaniApp.utils.view_utils import extract_latlongs
+
 
 def creatures(request):
     demcreature = Creature()  # placeholder
@@ -19,18 +21,19 @@ def creatures(request):
 
 def creature(request, creature_id):
     """
-    JSON format ; {"latlong" : [x,y]}
+    JSON format: [[61.757366, 22.344783], [61.757366, 22.344966]
     """
-    creature = get_object_or_404(Creature, pk=creature_id)
-    # demcreature = Creature()  # placeholder
-    # demcreature.name = "Koecreature"
 
-    points = [{"latlong": [float(mapPoint.latitude), float(mapPoint.longitude)]} for mapPoint in MapPoint.objects.filter(creature_id=creature_id, public=True)]
+    creature = get_object_or_404(Creature, pk=creature_id)
+
+    points = creature.return_public_points()
+
+    latlongs = extract_latlongs(points)
 
     context = {
         'creature_id': creature_id,
         'creature': creature,
-        'points': json.dumps(points)
+        'points': json.dumps(latlongs)
     }
 
     return render(request, 'papukaaniApp/creature.html', context)

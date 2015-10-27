9ab41ba8455167a3810c1306860e0d47e1e61e46
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2015-10-27 15:55:15 +0200

    Added forgotten Jasmine files

---
+++ spec/javascripts/choose_spec.js
@@ -0,0 +1,6 @@
+//This is just for testing purposes. Ok to remove.
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});

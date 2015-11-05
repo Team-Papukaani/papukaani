c3b3c79e718f887db20de7cccaf6b27a2758743f
Iiro Aalto <iiroaalto@gmail.com>
2015-11-05 15:09:59 +0200

    modified devices

--- papukaaniApp/views/device_views.py
+++ papukaaniApp/views/device_views.py
@@ -10,13 +10,20 @@ def devices(request):
     devices = device.get_all()
     individuals = individual.get_all()
 
+    # Directory, where key is deviceId and value is an array of individual data
+    individuals_of_devices = {device2.deviceId: device2.individuals for device2 in devices}
+
+    # Directory, where key is individualId and value is it's name (taxon)
+    individual_names = {individual2.individualId: individual2.taxon for individual2 in individuals}
+
     context = {
         'individuals': individuals,
-        'devices': devices
+        'devices': devices,
+        'device_json': json.dumps(individuals_of_devices),
+        'individual_json': json.dumps(individual_names)
     }
 
     return render(request, 'papukaaniApp/devices.html', context)
-
 
 _RESPONSE_BASE = {"errors" : []}
 

3a80c5e6b70393534302ddecee34ab3aa08792de
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-11-10 15:08:49 +0200

    speedslider added

--- papukaaniApp/static/papukaani/js/public.js
+++ papukaaniApp/static/papukaani/js/public.js
@@ -9,30 +9,39 @@ function PublicMap() {
     this.map = create_map("map", [61.0, 20.0], 5);
 }
 
-//Draws the polyline
-PublicMap.prototype.draw = function (points) {
-    var latlngs = this.createLatlngsFromPoints(points);
 
-    polylines = [];
-    i = 0;
-    window.setInterval(function () {
-        polyline = L.polyline([latlngs[i], latlngs[++i]], {color: 'blue', opacity: 1.0})
-        polylines.push(polyline);
-        polyline.addTo(this.map);
-        this.map.panTo(latlngs[i]);
-        if (polylines.length > 15) {
-            polylines.splice(0, 1);
-        }
-        for (j = 0; j < polylines.length; j++) {
-            polylines[j].setStyle({color: 'blue', opacity: (j / 15.0)});
-        }
-    }.bind(this), 700);
+//Draws the polyline animation. i = starting point
+PublicMap.prototype.animate = function (latlngs, i) {
+    return setTimeout(function () {
 
-};
+            polyline = L.polyline([latlngs[i], latlngs[++i]], {color: 'blue', opacity: 1.0})
+            this.polylines.push(polyline);
+            polyline.addTo(this.map);
+            this.map.panTo(latlngs[i]);
+            if (this.polylines.length > 15) {
+                this.polylines.splice(0, 1);
+            }
+            for (j = 0; j < this.polylines.length; j++) {
+                this.polylines[j].setStyle({color: 'blue', opacity: (j / 15.0)});
+            }
+
+            if(latlngs.length > (i+1)) {
+                this.animate(latlngs, i);
+            }
+
+
+        }.bind(this), 1000 - $('#speedSlider').slider("option", "value"))
+}
+
 
 //Redraws the polyline
 PublicMap.prototype.changePoints = function (points) {
-    this.draw(points);
+
+    var latlngs = this.createLatlngsFromPoints(points);
+
+    this.polylines = [];
+
+    var id = this.animate(latlngs, 0);
 };
 
 
@@ -55,3 +64,12 @@ function lockButtons() {
 function unlockButtons() {
     $("#selectDevice").attr("disabled", false);
 }
+
+//SpeedSlider settings
+$(function() {
+    $( "#speedSlider" ).slider({
+        value: 500,
+        min: 100,
+        max: 1000
+    });
+});

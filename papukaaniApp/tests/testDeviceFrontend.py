47642288ebaacbe14785c9154a65963aad2faa0c
Uskon <henri.vehmas@helsinki.fi>
2015-11-19 09:40:32 +0200

    Test duplicate clean-up

--- papukaaniApp/tests/testDeviceFrontend.py
+++ papukaaniApp/tests/testDeviceFrontend.py
@@ -43,11 +43,8 @@ class TestDeviceFrontend(StaticLiveServerTestCase):
 
     def test_if_unremoved_birds_attach_button_is_not_visible(self):
         self.assertFalse(self.page.ATTACHER.is_displayed())
-
-        self.page.REMOVE_TIME.send_keys("03-11-2015 00:00")
-        self.page.REMOVE.click()
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 00:00", self.page.ATTACHER.is_displayed(),
+                                                             True)
 
     def test_removed_individuals_are_not_selectable(self):
         self.I.deleted = True
@@ -56,79 +53,55 @@ class TestDeviceFrontend(StaticLiveServerTestCase):
             self.page.attach_individual("Birdie", "12-11-2015 00:00")
 
     def test_attacher_is_hidden_after_attach(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 00:00")
-        self.page.REMOVE.click()
-
-        self.page.attach_individual("12345TESTINDIVIDUAL", "12-11-2015 00:00")
-
-        self.assertFalse(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 00:00", self.page.ATTACHER.is_displayed(),
+                                                             False, "12345TESTINDIVIDUAL", "12-11-2015 00:00")
 
     def test_attacher_is_shown_after_remove(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 00:00")
-        self.page.REMOVE.click()
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 00:00", self.page.ATTACHER.is_displayed(),
+                                                             True)
 
     def test_cant_remove_if_remove_time_is_before_attach_time(self):
-        self.page.REMOVE_TIME.send_keys("01-10-2015 02:00")
-        self.page.REMOVE.click()
-
-        self.assertFalse(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("01-10-2015 02:00", self.page.ATTACHER.is_displayed(),
+                                                             False)
 
     def test_cant_remove_if_remove_time_is_in_the_future(self):
-        self.page.REMOVE_TIME.send_keys("13-12-2114 00:00")
-        self.page.REMOVE.click()
-
-        self.assertFalse(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("13-12-2114 00:00", self.page.ATTACHER.is_displayed(),
+                                                             False)
 
     def test_can_remove_when_all_conditions_are_met(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", True)
 
     def test_cant_attach_if_start_time_is_in_future(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.page.attach_individual("12345TESTINDIVIDUAL", "13-12-2114 00:00")
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", self.page.ATTACHER.is_displayed(),
+                                                             True, "12345TESTINDIVIDUAL", "13-12-2114 00:00")
 
     def test_cant_attach_if_start_time_overlaps_with_another_device(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.page.attach_individual("12345TESTINDIVIDUAL", "02-11-2015 16:00")
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", self.page.ATTACHER.is_displayed(),
+                                                             True, "12345TESTINDIVIDUAL", "02-11-2015 16:00")
 
     def test_can_attach_id_all_conditions_are_met(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.page.attach_individual("12345TESTINDIVIDUAL", "04-11-2015 16:00")
-
-        self.assertFalse(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", self.page.ATTACHER.is_displayed(),
+                                                             "12345TESTINDIVIDUAL", "04-11-2015 16:00")
 
     def test_errors_messages_are_shown_when_validation_fails(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.page.attach_individual("12345TESTINDIVIDUAL", "13-12-2114 00:00")
-
-        self.assertTrue(len(self.page.driver.find_element_by_id("errors").text) > 0)
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", (
+            len(self.page.driver.find_element_by_id("errors").text) > 0), True, "12345TESTINDIVIDUAL",
+                                                             "13-12-2114 00:00")
 
     def test_cant_attach_if_time_field_is_empty(self):
-        self.page.REMOVE_TIME.send_keys("03-11-2015 14:00")
-        self.page.REMOVE.click()
-
-        self.page.ATTACH.click()
-
-        self.assertTrue(self.page.ATTACHER.is_displayed())
+        self.perform_removal_and_assert_with_optional_attach("03-11-2015 14:00", self.page.ATTACHER.is_displayed(),
+                                                             True)
 
     def test_cant_remove_if_time_field_is_empty(self):
         self.page.REMOVE.click()
 
         self.assertFalse(self.page.ATTACHER.is_displayed())
 
+    def perform_removal_and_assert_with_optional_attach(self, time, result, expected, attachname=None, attachtime=None):
+        self.page.REMOVE_TIME.send_keys(time)
+        self.page.REMOVE.click()
+
+        if attachname and attachtime:
+            self.page.attach_individual(attachname, attachtime)
+
+        self.assertEquals(self.page.ATTACHER.is_displayed(), expected)

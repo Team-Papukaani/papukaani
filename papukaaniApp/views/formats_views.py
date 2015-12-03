87b30708631ad2fee8310c8e6f12e84139014bf0
leo0074 <leo-palkio@hotmail.com>
2015-12-03 15:00:14 +0200

    messages

--- papukaaniApp/views/formats_views.py
+++ papukaaniApp/views/formats_views.py
@@ -5,19 +5,27 @@ from django.contrib import messages
 
 @require_auth
 def formats(request):
+
     if request.method == 'POST':
         try:
             data = request.POST.copy().dict()
-            print(data)
 
-            if "csrfmiddlewaretoken" in data:
-                data.pop("csrfmiddlewaretoken")
-
-            GeneralParser.objects.create(**data)
-            messages.add_message(request, messages.ERROR, "Onnistui!")
+            if _check_parser_validity(data):
+                messages.add_message(request, messages.ERROR, "Pakollista tietoa puuttuu!")
+            else:
+                if "csrfmiddlewaretoken" in data:
+                    data.pop("csrfmiddlewaretoken")
+                GeneralParser.objects.create(**data)
+                messages.add_message(request, messages.INFO, "Onnistui!")
         except:
-            messages.add_message(request, messages.ERROR, "Error!!")
             raise ValueError("POST request does not contain required parameters!")
 
 
     return render(request, 'papukaaniApp/formats.html')
+
+def _check_parser_validity(parser):
+    print(parser)
+    if parser["formatName"] and parser["longitude"] and parser["gpsTime"] and parser["latitude"] and parser["delimiter"]:
+        return False
+    return True
+

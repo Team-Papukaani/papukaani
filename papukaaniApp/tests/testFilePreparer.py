5d10672eb6aebf62609d77578418af7deebee47b
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-12-08 18:06:07 +0200

    testfix

--- papukaaniApp/tests/testFilePreparer.py
+++ papukaaniApp/tests/testFilePreparer.py
@@ -26,23 +26,6 @@ class FilePreparerTest(TestCase):
         self.ecotone_parser.delete()
         self.byholm_parser.delete()
 
-    def test_points_can_be_succesfully_created_from_parsed_contents(self):
-        path = settings.OTHER_ROOT + "/Ecotones_gps_pos_test.csv"
-        file = open(path, "rb")
-        entries = prepare_file(file, self.ecotone_parser)
-        points = []
-        for entry in entries:
-            creature, was_created = Creature.objects.get_or_create(name="Pekka")
-            point = MapPoint(creature=creature,
-                             gpsNumber=entry['gpsNumber'],
-                             timestamp=entry['gpsTime'],
-                             latitude=entry['latitude'],
-                             longitude=entry['longitude'],
-                             altitude=entry['altitude'] if entry['altitude'] != '' else 0,
-                             temperature=entry['temperature'])
-            points.append(point)
-        assert len(points) == 5
-
     def test_byholm_file_parsing(self):
         path = settings.OTHER_ROOT + "/byholm_test.txt"
         file = open(path, "rb")
@@ -53,19 +36,3 @@ class FilePreparerTest(TestCase):
             assert float(lats[i]) == float(entry["latitude"])
             i += 1
 
-    def test_byholm_points_can_be_succesfully_created_from_parsed_contents(self):
-        path = settings.OTHER_ROOT + "/byholm_test.txt"
-        file = open(path, "rb")
-        entries = prepare_file(file, self.byholm_parser, "1010")
-        points = []
-        for entry in entries:
-            creature, was_created = Creature.objects.get_or_create(name="Pekka")
-            point = MapPoint(creature=creature,
-                             gpsNumber="0",
-                             timestamp=entry['gpsTime'],
-                             latitude=entry['latitude'],
-                             longitude=entry['longitude'],
-                             altitude=entry['altitude'] if entry['altitude'] != '' else 0,
-                             temperature="0")
-            points.append(point)
-        assert len(points) == 5

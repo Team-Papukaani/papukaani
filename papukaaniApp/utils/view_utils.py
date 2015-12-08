5ce319d0d44a860cc7dae41c6069a5fff8375499
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-12-08 15:31:44 +0200

    individual taxon is now the species info

--- papukaaniApp/utils/view_utils.py
+++ papukaaniApp/utils/view_utils.py
@@ -11,3 +11,13 @@ def extract_latlongs(points):
     latlongs = [[float(mapPoint.latitude), float(mapPoint.longitude)] for mapPoint in points]
     return latlongs
 
+
+def populate_facts(list):
+    """
+    Converts LajiStore facts into attributes for use in templates
+    """
+    for item in list:
+        if item.facts is None:
+            continue
+        for fact in item.facts:
+            setattr(item, fact['name'], fact['value'])

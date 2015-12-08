ef540ea8e9ab0ab39ccfcc2b2357e7f96ad9c70d
Iivo Raitahila <iivo.raitahila@cs.helsinki.fi>
2015-12-08 17:49:29 +0200

    cleaned code and fixed tests

--- papukaaniApp/tests/testFormatsView.py
+++ papukaaniApp/tests/testFormatsView.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from papukaaniApp.models import GeneralParser
 import time
+from django.conf import settings
 
 class TestFormatsView(TestCase):
 
     def setUp(self):
         self.c = Client()
-        self.url = "/papukaani/formats/"
+        self.url = "/papukaani/formats/0/"
+        GeneralParser.objects.all().delete()
+        settings.MOCK_AUTHENTICATION = "Skip"
 
 
     def test_post_to_formats_creates_a_GeneralParser(self):
@@ -18,6 +21,6 @@ class TestFormatsView(TestCase):
             "longitude" : "longitude",
             "delimiter" : ","
         })
-
+        
         self.assertEquals(len(GeneralParser.objects.all()), 1)
 
--- papukaaniApp/views/formats_views.py
+++ papukaaniApp/views/formats_views.py
@@ -4,22 +4,22 @@ from  papukaaniApp.services.laji_auth_service.require_auth import require_auth
 from django.core import serializers
 from django.contrib import messages
 
-@require_auth
-def formats(request):
-    if request.method == 'POST':
-        try:
-            data = request.POST.copy().dict()
-            print(data)
-
-            if "csrfmiddlewaretoken" in data:
-                data.pop("csrfmiddlewaretoken")
-
-            GeneralParser.objects.create(**data)
-        except:
-            raise ValueError("POST request does not contain required parameters!")
-
-
-    return render(request, 'papukaaniApp/formats.html')
+# @require_auth
+# def formats(request):
+#     if request.method == 'POST':
+#         try:
+#             data = request.POST.copy().dict()
+#             print(data)
+#
+#             if "csrfmiddlewaretoken" in data:
+#                 data.pop("csrfmiddlewaretoken")
+#
+#             GeneralParser.objects.create(**data)
+#         except:
+#             raise ValueError("POST request does not contain required parameters!")
+#
+#
+#     return render(request, 'papukaaniApp/formats.html')
 
 @require_auth
 def list_formats(request):
@@ -29,26 +29,33 @@ def list_formats(request):
 
 @require_auth
 def show_format(request, id):
-    parser = GeneralParser.objects.get(id=id)
 
-    if request.method == 'GET':
-        return render(request, "papukaaniApp/show_format.html", context={"format" : parser})
+    if request.method == 'GET' and int(id) > 0:
+        parser = GeneralParser.objects.get(id=id)
+        return render(request, "papukaaniApp/formats.html", context={"format" : parser})
 
     if request.method == 'POST':
         data = request.POST.copy().dict()
-        data.pop("csrfmiddlewaretoken")
+        if "csrfmiddlewaretoken" in data:
+            data.pop("csrfmiddlewaretoken")
 
         try:
-            for param in data:
-                setattr(parser, param, data[param])
-            parser.save()
-            messages.add_message(request, messages.SUCCESS, "Muutokset tallennettu!")
+            if int(id) > 0:
+                parser = GeneralParser.objects.get(id=id)
+                for param in data:
+                    setattr(parser, param, data[param])
+                parser.save()
+                messages.add_message(request, messages.SUCCESS, "Muutokset tallennettu!")
+            else:
+                GeneralParser.objects.create(**data)
+                messages.add_message(request, messages.SUCCESS, "Formaatti tallennettu!")
         except:
             messages.add_message(request, messages.ERROR, "Jokin meni pieleen!")
             return redirect(show_format)
 
         return redirect(list_formats)
 
+    return render(request, "papukaaniApp/formats.html")
 
 @require_auth
 def delete_format(request, id):

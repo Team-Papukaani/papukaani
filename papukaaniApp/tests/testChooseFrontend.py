7400b3ea25869b3c2ab352e6e7128c292d278578
Uskon <henri.vehmas@helsinki.fi>
2015-12-15 14:37:53 +0200

    Tests updated

--- papukaaniApp/tests/testChooseFrontend.py
+++ papukaaniApp/tests/testChooseFrontend.py
@@ -71,8 +71,8 @@ class TestChooseFrontend(StaticLiveServerTestCase):
         self.assertEquals(0, self.page.number_of_partially_public_clusters_on_map())
 
     def test_reset_button_clears_time_range_fields(self):
-        self.page.set_start_time("12-12-1234 00:00")
-        self.page.set_end_time("12-12-1234 00:00")
+        self.page.set_start_time("12.12.1234 00:00")
+        self.page.set_end_time("12.12.1234 00:00")
         self.page.reset()
         self.assertEquals(self.page.get_start_time(), '')
         self.assertEquals(self.page.get_end_time(), '')
@@ -83,14 +83,14 @@ class TestChooseFrontend(StaticLiveServerTestCase):
         self.assertEquals(0, self.page.number_of_private_clusters_on_map())
 
     def test_filtering_points_with_time_range(self):
-        self.page.set_start_time("12-12-1234 12:12")
-        self.page.set_end_time("12-12-1234 12:13")
+        self.page.set_start_time("12.12.1234 12:12")
+        self.page.set_end_time("12.12.1234 12:13")
         self.page.show_time_range()
         self.assertEquals(self.page.get_cluster_size(), "0/1")
 
     def test_filtering_with_end_time_starting_before_start_time_returns_no_points(self):
-        self.page.set_start_time("13-12-1234 00:00")
-        self.page.set_end_time("12-12-1234 00:00")
+        self.page.set_start_time("13.12.1234 00:00")
+        self.page.set_end_time("12.12.1234 00:00")
         self.page.show_time_range()
         self.assertEquals(self.page.number_of_private_clusters_on_map(), 0)
 

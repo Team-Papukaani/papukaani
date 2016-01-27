fe2d5d19f5c0563cdef1cf6c228080ea8da3a3d6
Jim Saartia <jim@saartia.fi>
2016-01-27 22:19:02 +0200

    redirected root url to app

--- papukaani/urls.py
+++ papukaani/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^papukaani/', include('papukaaniApp.urls'))
+    url(r'^papukaani/', include('papukaaniApp.urls')),
+    url(r'^$', RedirectView.as_view(url='/papukaani/', permanent=True))
 ]

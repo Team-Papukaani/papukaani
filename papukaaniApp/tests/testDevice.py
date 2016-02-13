f662a7c96a590d37eaa6beec8c85875359204190
Jim Saartia <jim@saartia.fi>
2016-02-13 17:37:44 +0200

    fixed tests

--- papukaaniApp/tests/testDevice.py
+++ papukaaniApp/tests/testDevice.py
@@ -1,9 +1,6 @@
 from django.test import TestCase
 from django.test import Client
-from papukaaniApp.models_LajiStore import document, gathering, device, individual
-from papukaaniApp.models import *
-from datetime import datetime
-import json
+from papukaaniApp.models_LajiStore import device, individual
 
 _URL = '/papukaani/devices/'
 
@@ -11,8 +8,8 @@ _URL = '/papukaani/devices/'
 class TestDevice(TestCase):
     def setUp(self):
         self.c = Client()
-        self.A = device.create('1234TEST_A',"type","Manufacturer1","2015-10-27T16:32:01+00:00", "2015-10-27T16:32:01+00:00")
-        self.B = device.create("1234TEST_B","type","Manufacturer2","2015-10-27T16:32:01+00:00", "2015-10-27T16:32:01+00:00")
+        self.A = device.create("type","Manufacturer1",'1234TEST_A',"2015-10-27T16:32:01+00:00", "2015-10-27T16:32:01+00:00")
+        self.B = device.create("type","Manufacturer2","1234TEST_B","2015-10-27T16:32:01+00:00", "2015-10-27T16:32:01+00:00")
         self.indiv = individual.create("Lintu1","Tax")
 
     def tearDown(self):
@@ -22,29 +19,25 @@ class TestDevice(TestCase):
 
     def test_devices_are_listed(self):
         response = self.c.get(_URL)
-        self.assertTrue("Manufacturer1" in str(response.content))
-        self.assertTrue("Manufacturer2" in str(response.content))
+        self.assertTrue("1234TEST_A" in str(response.content))
+        self.assertTrue("1234TEST_B" in str(response.content))
 
     def test_post_to_attach_attaches_individual(self):
 
-        response = self.c.post(_URL + "1234TEST_A/attach/", data={
-            "individualId": self.indiv.ringID,
-            "attached": "2015-10-10T10:10:10+00:00"
+        response = self.c.post(_URL + self.A.id + "/attach/", data={
+            "individualId": self.indiv.id,
+            "timestamp": "2015-10-10T10:10:10+00:00"
         })
 
-        '''self.A = device.find(deviceId=self.A.deviceId)[0]'''
-
-        self.assertEquals(1, len(self.A.individuals))
+        self.assertNotEquals(None, self.A.get_attached_individualid())
 
     def test_post_to_remove_removes_individual(self):
-        self.A.attach_to(self.indiv,"2015-10-10T10:10:10+00:00" )
+        self.A.attach_to(self.indiv.id, "2015-10-10T10:10:10+00:00" )
 
-        response = self.c.post(_URL + "1234TEST_A/remove/", data={
-            "individualId" : self.indiv.individualId,
+        response = self.c.post(_URL + self.A.id + "/remove/", data={
+            "individualId" : self.indiv.id,
             "timestamp" : "2015-10-10T10:10:10+00:00"
         })
 
-        self.A = device.find(deviceId=self.A.deviceId)[0]
-
-        self.assertEquals(0, len(self.A.individuals))
+        self.assertEquals(None, self.A.get_attached_individualid())
 

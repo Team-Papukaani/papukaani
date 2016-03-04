4fe48c919a804144e0f73ea463e78edb3a0dfdab
xuegangchai <xuegang.chai@helsinki.fi>
2016-03-04 02:12:54 +0200

    kenttien validointi

--- papukaaniApp/static/papukaani/js/individuals.js
+++ papukaaniApp/static/papukaani/js/individuals.js
@@ -17,11 +17,68 @@ function confirmdelete(form) {
     popup.show()
 };
 
+tinymce.init({
+    selector: 'textarea', // change this value according to your HTML
+    elementpath: false,
+    height: 300,
+    resize: false,
+    menubar: false
+});
+
+$(function validate() {
+        $("#new_individual_form").validate({
+            rules: {
+                id: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                taxon: {
+                    required: true
+                }
+            }
+        });
+        $("#modify_individuals").validate({
+             errorPlacement: function(error, element) {
+                if (element.attr("name") == "nickname" || element.attr("name") == "taxon" ) {
+                    error.insertAfter("#luo");
+                } else {
+                    error.insertAfter(element);
+                }
+             },
+            rules: {
+                id: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                taxon: {
+                    required: true
+                },
+                ring_id: {
+                    number: true
+                },
+                descriptionUrlEN: {
+                    url: true
+                },
+                descriptionUrlFI: {
+                    url: true
+                },
+                descriptionUrlSV: {
+                    url: true
+                }
+            }
+        });
+});
+
+
 $(document).ready(function () {
+    runtinymce();
     $('.combobox').combobox();
 
     $("button.confirmdelete").click(function (e) {
         e.preventDefault();
         confirmdelete($(this).closest('form'));
     });
+    validate();
 });

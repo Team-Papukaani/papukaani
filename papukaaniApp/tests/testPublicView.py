c020c4af8e4e5499fda387a80d47cab84627a1d3
Jim Saartia <jim@saartia.fi>
2016-03-07 18:31:41 +0200

    less sleeps

--- papukaaniApp/tests/testPublicView.py
+++ papukaaniApp/tests/testPublicView.py
@@ -208,6 +208,8 @@ class PublicView(StaticLiveServerTestCase):
         self.page.TIME_END.send_keys("14.12.2010 00:00")
         # just to defocus (blur) previous field
         self.page.driver.find_element_by_id("playLabel").click()
+        print(self.page.driver.find_element_by_id("playLabel").text)
+        print(self.page.driver.find_element_by_id("playLabel_end").text)
         self.assertTrue("10.12.2010" in self.page.driver.find_element_by_id("playLabel").text)
         self.assertTrue("14.12.2010" in self.page.driver.find_element_by_id("playLabel_end").text)
 

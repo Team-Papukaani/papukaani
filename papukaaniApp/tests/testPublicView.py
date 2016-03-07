a6ff41d5bba7e584f1532feb4964e86be306a308
Jim Saartia <jim@saartia.fi>
2016-03-07 09:43:35 +0200

    added more sleeps

--- papukaaniApp/tests/testPublicView.py
+++ papukaaniApp/tests/testPublicView.py
@@ -190,23 +190,24 @@ class PublicView(StaticLiveServerTestCase):
 
     def test_time_selection_shows_correct_points(self):
         self.page.TIME_START.send_keys("10.12.2010 00:00")
-        time.sleep(1)
+        time.sleep(3)
         self.page.TIME_END.send_keys("14.12.2010 00:00")
-        time.sleep(1)
+        time.sleep(3)
         # just to defocus (blur) previous field
         self.page.driver.find_element_by_id("playLabel").click()
-        time.sleep(3)
+        time.sleep(5)
         self.page.change_individual_selection(str(self.I.id))
+        time.sleep(3)
         self.assertTrue("10.12.2010" in self.page.driver.find_element_by_id("playLabel").text)
         self.assertTrue("14.12.2010" in self.page.driver.find_element_by_id("playLabel_end").text)
 
     def test_time_selection_refresh_works(self):
         self.page.change_individual_selection(str(self.I.id))
-        time.sleep(1)
+        time.sleep(3)
         self.page.TIME_START.send_keys("10.12.2010 00:00")
-        time.sleep(1)
+        time.sleep(3)
         self.page.TIME_END.send_keys("14.12.2010 00:00")
-        time.sleep(1)
+        time.sleep(3)
         # just to defocus (blur) previous field
         self.page.driver.find_element_by_id("playLabel").click()
         time.sleep(5)

8d3f6d7902dfe933a9b16f010bccf8f2868b9442
Jim Saartia <jim@saartia.fi>
2016-03-20 18:27:25 +0200

    added routing, controllers and javascript for basic ajax-interface

--- papukaaniApp/static/papukaani/js/news.js
+++ papukaaniApp/static/papukaani/js/news.js
@@ -1,3 +1,106 @@
-$(function(){
+$(function () {
+    load_news();
+
+    $("#newslist").on("click", "button.remove", function (e) {
+        e.preventDefault();
+        delete_news($(this).data("id"));
+    });
+    $("#newslist").on("click", "button.read", function (e) {
+        e.preventDefault();
+        read_news($(this).data("id"));
+    });
+    $("#newslist").on("click", "button.update", function (e) {
+        e.preventDefault();
+        update_news($(this).data("id"));
+    });
+
+    $("#createnews").click(function (e) {
+        e.preventDefault();
+        create_news();
+    });
 
 });
+
+function create_news() {
+    var postdata = {
+        title: 'moi',
+        language: 'fi',
+        content: 'test'
+    };
+    $.post('/papukaani/news/', postdata, function (data) {
+        if (data.status === 'OK') {
+            load_news();
+        } else {
+            alert(data.errors);
+        }
+    }, 'json');
+}
+function update_news(id) {
+    var postdata = {
+        title: 'updated',
+        language: 'sv',
+        content: 'päivitetty uutinen',
+        targets: [1234, 1234]
+    };
+    $.ajax({
+        url: '/papukaani/news/' + id,
+        type: 'PUT',
+        data: postdata,
+        success: function (data) {
+            if (data.status === "OK") {
+                load_news();
+            } else {
+                alert(data.errors);
+            }
+        },
+        dataType: "json"
+    });
+}
+function delete_news(id) {
+    $.ajax({
+        url: '/papukaani/news/' + id,
+        type: 'DELETE',
+        success: function (data) {
+            if (data.status === "OK") {
+                load_news();
+            } else {
+                alert(data.errors);
+            }
+        },
+        dataType: "json"
+    });
+}
+
+function read_news(id) {
+    $.get('/papukaani/news/' + id, function (data) {
+        var n = data.news;
+        alert("otsikko: " + n.title);
+    }, "json");
+}
+
+function load_news() {
+    $.get('/papukaani/news/list', function (data) {
+        var list = $("#newslist tbody");
+        var html = [];
+        $.each(data.news, function (i, v) {
+            html.push($('<tr></tr>').append(
+                $('<td></td>').text(v.id)
+            ).append(
+                $('<td></td>').text(v.title)
+            ).append(
+                $('<td></td>').text(v.language)
+            ).append(
+                $('<td></td>').text(v.publishDate)
+            ).append(
+                $('<td></td>').text(v.targets)
+            ).append(
+                $('<td><button class="remove" data-id="' + v.id + '">Poista</button></td>')
+            ).append(
+                $('<td><button class="read" data-id="' + v.id + '">Avaa</button></td>')
+            ).append(
+                $('<td><button class="update" data-id="' + v.id + '">Muokkaa</button></td>')
+            ));
+        });
+        list.html(html)
+    }, "json");
+}
--- papukaaniApp/views/news_views.py
+++ papukaaniApp/views/news_views.py
@@ -1,19 +1,188 @@
-from  papukaaniApp.services.laji_auth_service.require_auth import require_auth
-from django.shortcuts import render
-from papukaaniApp.models_LajiStore import individual
-from papukaaniApp.models_TipuApi import species
+from django.http import QueryDict
 from papukaaniApp.views.decorators import count_lajistore_requests
-from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
+from papukaaniApp.services.laji_auth_service.require_auth import require_auth
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+from papukaaniApp.models_LajiStore import news
+from papukaaniApp.models_LajiStore import individual
+
+_RESPONSE_BASE = {"errors": None, "status": "OK"}
 
 
 @count_lajistore_requests
+@api_view(['GET', 'POST'])
 @require_auth
-def news(request):
+def news_index(request):
+    if request.method == 'POST':
+        return _create(request)
+    return _index(request)
+
+
+@count_lajistore_requests
+@api_view(['GET'])
+@require_auth
+def news_list(request):
+    return _list(request)
+
+
+@count_lajistore_requests
+@api_view(['GET', 'PUT', 'DELETE'])
+@require_auth
+def news_rest(request, id):
+    if request.method == 'PUT':
+        return _update(request, id)
+    if request.method == 'DELETE':
+        return _delete(request, id)
+    return _read(request, id)
+
+
+def _index(request):
     """
-    Controller for '/news/'. GET renders view
+    Controller for '/news/'. GET renders view with template
     """
-    context = {
-        'test': "testi"
-    }
+    context = {}
     return render(request, 'papukaaniApp/news.html', context)
+
+
+def _list(request):
+    """
+    Controller for '/news/list'. GET returns json list of news
+    """
+    response = _RESPONSE_BASE.copy()
+    response["news"] = {}
+    for n in news.find():
+        response["news"][n.id] = {
+            "id": n.id,
+            "title": n.title,
+            "content": n.content,
+            "language": n.language,
+            "publishDate": n.publishDate,
+            "targets": n.targets
+        }
+    return Response(response)
+
+
+def _create(request):
+    """
+    Controller for '/news/'. POST create new piece of news
+    """
+    response = _RESPONSE_BASE.copy()
+    missing = _check_missing_params(request.POST, 'title', 'language', 'content')
+    _add_error(response, missing)
+    if _has_errors(response):
+        return Response(response)
+
+    publishDate = None if "publishDate" not in request.POST else request.POST["publishDate"]
+    targets = None if "targets" not in request.POST else request.POST["targets"]
+
+    try:
+        n = news.create(request.POST["title"], request.POST["content"], request.POST["language"], publishDate, targets)
+    except Exception as e:
+        _add_error(response, e)
+        return Response(response)
+
+    response["news"] = {}
+    response["news"][n.id] = {
+        "id": n.id,
+        "title": n.title,
+        "content": n.content,
+        "language": n.language,
+        "publishDate": n.publishDate,
+        "targets": n.targets
+    }
+
+    return Response(response)
+
+
+def _read(request, id):
+    """
+    Controller for '/news/:id'. GET news by id
+    """
+    response = _RESPONSE_BASE.copy()
+    n = news.get(id)
+    response["news"] = {}
+    if n is not None:
+        response["news"] = {
+            "id": n.id,
+            "title": n.title,
+            "content": n.content,
+            "language": n.language,
+            "publishDate": n.publishDate,
+            "targets": n.targets
+        }
+    else:
+        _add_error(response, "Not found")
+    return Response(response)
+
+
+def _update(request, id):
+    """
+    Controller for '/news/:id'. PUT (update) news by id
+    """
+    response = _RESPONSE_BASE.copy()
+    n = news.get(id)
+    response["news"] = {}
+    if n is None:
+        _add_error(response, "Not found")
+        return Response(response)
+
+    PUT = QueryDict(request.body)
+
+    missing = _check_missing_params(PUT, 'title', 'language', 'content')
+    _add_error(response, missing)
+    if _has_errors(response):
+        return Response(response)
+
+    n.title = PUT["title"]
+    n.language = PUT["language"]
+    n.content = PUT["content"]
+    n.publishDate = None if "publishDate" not in PUT else PUT["publishDate"]
+    n.targets = [] if "targets" not in PUT else PUT["targets"]
+    n.update()
+
+    response["news"] = {
+        "id": n.id,
+        "title": n.title,
+        "content": n.content,
+        "language": n.language,
+        "publishDate": n.publishDate,
+        "targets": n.targets
+    }
+    return Response(response)
+
+
+def _delete(request, id):
+    """
+    Controller for '/news/:id'. DELETE news by id
+    """
+    response = _RESPONSE_BASE.copy()
+    n = news.get(id)
+    if n is not None:
+        n.delete()
+    else:
+        _add_error(response, "Not found" + id)
+    return Response(response)
+
+
+def _check_missing_params(collection, *args):
+    missing = []
+    for param in args:
+        if not param in collection:
+            missing.append("Missing parameter: " + str(param))
+    return missing
+
+
+def _add_error(response, error):
+    if not error:
+        return
+    if response["errors"] is None:
+        response["errors"] = []
+    response["errors"].append(error)
+    response["status"] = "ERROR"
+
+
+def _has_errors(response):
+    if response["errors"] is None:
+        return False
+    return len(response["errors"]) > 0

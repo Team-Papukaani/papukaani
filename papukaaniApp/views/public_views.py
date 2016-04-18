fc1bc7534af404f380af8488838f8aea23cafd73
Jim Saartia <jim@saartia.fi>
2016-04-18 16:30:58 +0300

    realized djangos decorator cache is full of cr*p

--- papukaaniApp/views/public_views.py
+++ papukaaniApp/views/public_views.py
@@ -1,7 +1,6 @@
 import json
 
 from django.shortcuts import render, get_object_or_404
-from django.views.decorators.cache import cache_page
 
 from papukaaniApp.models import *
 from papukaaniApp.models_TipuApi import species
@@ -11,8 +10,6 @@ from papukaaniApp.models_LajiStore import *
 from papukaaniApp.views.login_view import *
 from papukaaniApp.views.decorators import count_lajistore_requests
 
-
-@cache_page(60 * 15, cache="public")
 @count_lajistore_requests
 @xframe_options_exempt  # Allows the view to be loaded in an iFrame
 def public(request):
@@ -22,14 +19,15 @@ def public(request):
     individuals = dict()
     inds_objects = individual.find_exclude_deleted()
 
+
     for individuale in inds_objects:
+
         key = individuale.taxon
         individuals.setdefault(key, [])
         individualInfo = {'nickname': individuale.nickname,
                           'description': individuale.description,
                           'descriptionURL': individuale.descriptionURL,
-                          'news': json.dumps(news.find_by_individual_and_language(individualID=individuale.id,
-                                                                                  language=request.LANGUAGE_CODE))
+                          'news' : json.dumps(news.find_by_individual_and_language(individualID=individuale.id, language=request.LANGUAGE_CODE), default=set_default)
                           }
 
         individuals[key].append({individuale.id: individualInfo})
@@ -62,3 +60,8 @@ def public(request):
                }
 
     return render(request, 'papukaaniApp/public.html', context)
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    return obj.__dict__

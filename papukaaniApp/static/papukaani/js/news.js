c8e2205e63e62183c56db24dc62184f482156bfc
xuegangchai <xuegang.chai@helsinki.fi>
2016-04-21 15:24:06 +0300

    tinymce refaktorointi ja js-tiedostot paikalliseksi tiedostoiksi

--- papukaaniApp/static/papukaani/js/news.js
+++ papukaaniApp/static/papukaani/js/news.js
@@ -70,23 +70,6 @@ function displayTimeWithLeadingZeroes(time) {
     return day + "." + month + "." + d.getFullYear() + " " + hours + ":" + minutes;
 }
 
-tinymce.init({
-    selector: 'textarea', // change this value according to your HTML
-    elementpath: false,
-    height: 300,
-    resize: false,
-    plugins: [
-    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars code fullscreen',
-    'insertdatetime media nonbreaking save table contextmenu directionality',
-    'template paste textcolor colorpicker textpattern imagetools'
-   ],
-   toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor | link image print preview',
-    image_description: false,
-    target_list: false,
-    link_title: false
-});
-
 function save_news(id) {
     var postdata = {
         title: $('#news_title').val(),
@@ -326,9 +309,3 @@ function showLoadingBar() {
 function hideLoadingBar() {
     $('#loading').modal('hide');
 }
-
-$(document).on('focusin', function(e) {
-        if ($(e.target).closest(".mce-window").length) {
-            e.stopImmediatePropagation();
-        }
-});
